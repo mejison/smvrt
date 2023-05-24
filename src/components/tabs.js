d9a61cd179a30402da8cfeda88d31616ceeb0b18
mejison <dev.mejison@gmail.com>
2023-05-24 12:37:33 +0300

    archive projects details page

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,6 +1,6 @@
 export default function Tabs(props) {
-    const { tabs, active, change } = props
-    return (<div className="flex text-[#404040]">
+    const { tabs, active, change, className } = props
+    return (<div className={`flex text-[#404040] ${className}`}>
         {
             tabs.map(function(tab, index) {
                 return (<a href="#" key={index} onClick={() => change(tab)} className={`mr-6 ${active.slug == tab.slug ? 'border-b-[#4ECFE0]' : 'border-b-[#E5E5E5]'} border-b-[6px] p-3`}>
