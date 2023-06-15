cfb20a06c3f2726bec236b53df8ff7782aa7c06f
mejison <dev.mejison@gmail.com>
2023-06-15 12:12:36 +0300

    5) Step 3 - Approvers and Signatures

--- src/app/new-project/step-3/page.js
+++ src/app/new-project/step-3/page.js
@@ -108,9 +108,9 @@ export default function StepThree() {
                 
                 <div>
                     <label className="font-Eina03 text-[12px] text-[#222] flex items-center">
-                        <div className={`w-[18px] h-[18px] ${project.save_for_future == false ? 'bg-[#4ECFE0]' : 'border-2 border-[#D4D4D4]'} rounded-[3px] text-white flex items-center justify-center`}>
+                        <div className={`w-[18px] h-[18px] ${project.save_for_future != false ? 'bg-[#4ECFE0]' : 'border-2 border-[#D4D4D4]'} rounded-[3px] text-white flex items-center justify-center`}>
                             {
-                                ! project.save_for_future ? (
+                                project.save_for_future ? (
                                     <span>
                                         <svg width="12" height="10" viewBox="0 0 12 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fillRule="evenodd" clipRule="evenodd" d="M10.7851 1.31402C10.9568 1.47146 10.9684 1.73832 10.811 1.91007L4.62349 8.66007C4.54573 8.7449 4.43671 8.79428 4.32166 8.79678C4.20662 8.79928 4.09555 8.75468 4.01419 8.67331L1.20169 5.86081C1.03694 5.69606 1.03694 5.42894 1.20169 5.26419C1.36644 5.09944 1.63356 5.09944 1.79831 5.26419L4.29925 7.76513L10.189 1.33993C10.3465 1.16818 10.6133 1.15658 10.7851 1.31402Z" fill="white" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
@@ -119,7 +119,7 @@ export default function StepThree() {
                                 ) : <></>
                             }
                         </div>
-                        <span className="px-[12px] py-[26px]" onClick={() => setProject({...project, save_for_future: ! project.save_for_future})}>
+                        <span className="px-[12px] py-[26px] cursor-pointer" onClick={() => setProject({...project, save_for_future: ! project.save_for_future})}>
                             Save <strong>Approvers</strong> and <strong>Signatories</strong> for this team for future projects
                         </span>
                     </label>
